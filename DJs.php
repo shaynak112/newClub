30412d9ae8ac62540aad43d5edfea6e9e68c9f27
Shayna <shaynak112@gmail.com>
2017-12-20 13:34:06 -0500

    Add files via upload
    updated DJs with hovering starting to work; onclick does not yet work; galleries has just pulled from the database, need to implement the slideshow; index just made some small formatting changes

--- DJs.php
+++ DJs.php
@@ -9,13 +9,14 @@ include 'header.php';
 
 ?>
 
-<body>
+<script src="scripts/script.js"></script>    
 
 <div id='container'>
 <h1>DJs</h1>
 
 <p>Toronto has many talented and unique DJs who spin a variety of styles.</p>
-<p>this will be based on a database</p>
+<p>action onhover: image changes</p>
+<p>action onclick: links and bio are given</p>
 
 		<?php
 
@@ -36,7 +37,9 @@ include 'header.php';
 		{
 			echo "<div class='oneDJDiv' style='width:20%;margin-right:1%;display:inline-block;vertical-align:top;'>";
 			echo "<h2>" . $p->djName . "</h2>";
-			echo "<img class='firstImg' src='djs/" . $p->djID . "/img1.jpg' style='width:80%;margin-left:10%;'>";
+			echo "<img class='firstDJImg' src='djs/" . $p->djID . "/img1.jpg' style='width:80%;margin-left:10%;'>";
+			echo "<img class='secondDJImg' src='djs/" . $p->djID . "/img2.jpg' style='width:80%;margin-left:10%;display:none;'>";
+			echo "<p style='display:none;' class='DJinfo'>Some information</p>";
 			//echo "<img class='secondImg' src='djs/" . $p->djID . "/img2.jpg'>";
 			echo "</div>";
 
@@ -49,6 +52,67 @@ include 'header.php';
 </div> <!--end container-->
 
 
+
+
+<script>
+	$('.firstDJImg').hover(function()
+  {
+//possibly make visibility css and then change this visbility???? would that work???
+    $('.firstDJImg').show();
+    //$(this).next().css("width", "20%");
+    
+    //$(this).next('.firstDJImg').hide();
+    //$(this).next('.firstDJImg').css("border-color", "#FF66CC");
+    
+    //$(this).next('.firstDJImg').css("width", "20%");
+    //$(this).next('.firstDJImg').css("display", "none");
+    //$(this).next('.firstDJImg').css("visibility", "hidden");
+    //$('.firstDJImg').hide();
+    //$('.firstDJImg').css("border-color", "#32CD32");
+    $('.secondDJImg').hide();
+    $(this).next('.secondDJImg').show();
+    $('.secondDJImg').css("z-index", "-1");
+    $('.secondDJImg').css("width", "80%");
+    $('.secondDJImg').css("padding-top", "0px");
+
+    //$(this).css("width", "20%;");
+    //$(this).css("border-color", "#FF66CC");
+
+  }
+
+  );
+
+	</script>
+
+	<script>/*
+
+$('.firstDJImg').click(function()
+	{
+
+	$('.DJinfo').hide();
+    $(this).next('.DJinfo').show();
+
+	}
+
+);*/
+
+</script>
+
+<script>/*
+$('.aboutImage').click(function()
+	{
+
+	$('.DJinfo').hide();
+    $('.aboutImage').css("border-color", "#32CD32");
+    $(this).next('.DJinfo').show();
+    $(this).css("border-color", "#FF66CC");
+
+	}
+
+	);
+
+  */
+</script>
 
 </body>
 
--- gallery.php
+++ gallery.php
@@ -1,6 +1,9 @@
-<?php
+<?php $pjGroup = "1";
+ include '/home/dh_ev2582/findersoftheuniverse.com/portal/test/scripts/app/views/pjLayouts/pjActionProtect.php'; ?>
 
-//include 'dbConnect.php';
+ <?php
+
+include 'dbConnect.php';
 include 'nav.php';
 include 'header.php';
 
@@ -8,20 +11,63 @@ include 'header.php';
 
 <body>
 
-<div id='container'>
 <h1>Gallery</h1>
 
-<p>this will be based on a database (BLOB images)</p>
+<p>Going to put images in an array and then a slideshow. Perhaps some links or something at the bottom with thumbnails but the main part will be the slideshow.</p>
 
-</div>
+		<?php
 
+            $dbconn = new Dbconnect;
+            $db = $dbconn->getDb();
+            $query = "SELECT * FROM images ORDER BY imageID DESC ";
+            $statement = $db->prepare($query);
+            $statement->execute();
+            $allImgs = $statement->fetchAll(PDO::FETCH_OBJ);
+		?>
 
+	<?php
+		foreach($allImgs as $p)
+		{
+			echo "<div style='width:20%;margin-right:1%;display:inline-block;vertical-align:top;'>";
+			echo "<img class='galleryImage' src='gallery/" . $p->imageLocation . "' style='width:80%;margin-left:10%;'>";
+			echo "<p>" . $p->imageEventName . "</p>";
+			echo "<p>" . $p->imageDescription . "</p>";
+			echo "</div>";
+
+		}
+	?>
+<script>
+/*
+  var myIndex = 0;
+  var index2 = 1;
+
+  function gallerySlideshow()
+  {
+      var y;
+      var x = document.getElementsByClassName("slides");
+      for (y = 0; y < x.length; y++)
+      {
+         x[y].style.display = "none";  
+      }
+
+      myIndex++;
+      
+      if (myIndex > x.length)
+        {
+          myIndex = 1;
+        }    
+
+      x[myIndex-1].style.display = "block";  
+      setTimeout(gallerySlideshow, 4000);
+  }
+
+  
+
+  gallerySlideshow();
+
+*/
+
+</script>
 
 </body>
-
-
-<?php
-
-	//include 'footer.php';
-
-?>
+</html>
